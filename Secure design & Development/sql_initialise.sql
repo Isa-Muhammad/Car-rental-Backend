ad829c25ca74c70593832a14a4bbd0575529e0f4
Isa-Muhammad <36093501+Isa-Muhammad@users.noreply.github.com>
2019-12-19 01:17:18 +0000

    Bug fixes

--- Secure design & Development/sql_initialise.sql
+++ Secure design & Development/sql_initialise.sql
@@ -10,3 +10,8 @@ create database if not exists Autoshop;
 
 /* Car INSERTS*/
 insert ignore into Autoshop.cars values (1,"Honda Civic",10000,'/images/honda-civic-type-r.JPG');
+insert ignore into Autoshop.cars values (2,"Toyota C-HR",10000,'/images/toyota-c-hr.JPG');
+insert ignore into Autoshop.cars values (3,"Toyota Avalon",10000,'/images/Avalon-Hybrid.JPG');
+insert ignore into Autoshop.cars values (4,"Lamborghini",30000,'/images/Lambo.JPG');
+insert ignore into Autoshop.cars values (5,"Ferrari SF90",50000,'/images/Ferrari_SF90_Stradale.JPG');
+insert ignore into Autoshop.cars values (6,"Cyber truck",49000,'/images/cybertruck.JPG');
